c9e9a9451dc806674047b4fe08287c584bc57104
AlexeyYakovec <yakovec8952@gmail.com>
2024-06-05 00:34:12 +0500

    added slider

--- src/components/SliderComponent/SliderComponent.jsx
+++ src/components/SliderComponent/SliderComponent.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import "./slider.scss";
 
-function SliderComponent() {
-   return <div>slider</div>;
+function SliderComponent({ images }) {
+   const [imageIndex, setImageIndex] = useState(null);
+   return (
+      <div className="slider">
+         {imageIndex !== null && (
+            <div className="fullSlider">
+               <div className="arrow">
+                  <img src="/arrow.png" alt="" />
+               </div>
+               <div className="imgContainer">
+                  <img src={images[0]} alt="" />
+               </div>
+               <div className="arrow">
+                  <img src="/arrow.png" alt="" className="arrow-right" />
+               </div>
+               <div className="close">X</div>
+            </div>
+         )}
+         <div className="bigImage">
+            <img src={images[0]} alt="" />
+         </div>
+         <div className="smallImages">
+            {images.slice(1).map((image, index) => (
+               <img src={image} alt="" key={index} />
+            ))}
+         </div>
+      </div>
+   );
 }
 
 export default SliderComponent;
